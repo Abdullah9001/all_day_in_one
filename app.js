4fb186f68cc17b9a370270121af4202c3da127d5
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-06-08 00:38:21 +0600

    Javscript class lesson done

--- app.js
+++ app.js
@@ -72,3 +72,44 @@ console.log(helloWorld());
 
 // you can also print like this way
 const helloPrint = () => "Hello World";
+
+// Constactor class
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age() {
+    let date = new Date();
+    console.log(date.getFullYear() - this.year);
+  }
+}
+
+const car1 = new Car("Toyota", 2009);
+const car2 = new Car("BMW", 2017);
+const car3 = new Car("Maruti", 2006);
+
+car1.age();
+car2.age();
+car3.age();
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
+
+class Person {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  play(status) {
+    console.log(`${this.name} is playing ${status}`);
+  }
+}
+
+const persons1 = new Person("Sakib", 22);
+const persons2 = new Person("Rakib", 24);
+
+persons1.play("well");
+persons2.play("bad");
+// Javscript class lesson done
