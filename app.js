66299ab10ef8b8ddf1d3f87b0b00d21f5e46b5ef
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-06-08 02:07:35 +0600

    done for today

--- app.js
+++ app.js
@@ -136,3 +136,39 @@ const jsonData2 = [
 const json = JSON.stringify(jsonData2);
 
 console.log(json);
+
+// Just try it now
+const date = new Date();
+const day = date.getDay();
+
+let dayName;
+
+switch (day) {
+  case 0:
+    dayName = "Sunday";
+    break;
+  case 1:
+    dayName = "Monday";
+    break;
+  case 2:
+    dayName = "Tuesday";
+    break;
+  case 3:
+    dayName = "Wednesday";
+    break;
+  case 4:
+    dayName = "Thursday";
+    break;
+  case 5:
+    dayName = "Friday";
+    break;
+  case 6:
+    dayName = "Saturday";
+    break;
+  default:
+    dayName = "Unknown";
+}
+
+console.log("Today is " + dayName);
+
+// done for today
